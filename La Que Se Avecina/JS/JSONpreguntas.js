8520e601cbbab29bb70d759842b714868dc4c109
Adrian Jacek <adrianjacek@iesluisbraille.es>
2024-01-17 10:57:05 +0100

    Commit JSON

---
+++ La Que Se Avecina/JS/JSONpreguntas.js
@@ -0,0 +1,37 @@
+
+
+let preguntas =[
+
+
+    {
+        pregunta:"Fumas sustancias estupefacientes?",
+        respuestas:
+            [
+            {respuesta:"Uno al año no hace daño",puntos:20},
+            {respuesta:"Nunca",puntos:10},
+            {respuesta:"Siempre",puntos:30},
+            {respuesta:"De vez en cuando",puntos:10},
+            ]
+    },
+
+    {
+        pregunta:"Le pegarias un centollazo al presidente?",
+        respuestas:
+            [
+                {respuesta:"Se lo merece el cabron",puntos:30},
+                {respuesta:"No",puntos:10},
+            ]
+    },
+
+    {
+        pregunta:"Cual seria tu oficio?",
+        respuestas:
+            [
+                {respuesta:"Concejal",puntos:30},
+                {respuesta:"Nunca",puntos:10},
+                {respuesta:"Banquero",puntos:30},
+                {respuesta:"Psicologo",puntos:30},
+            ]
+    }
+
+]

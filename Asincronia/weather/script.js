cf6c3d436d311bb39cd20ff98dca1f8c77b6bd43
Adrian Jacek <adrianjacek@iesluisbraille.es>
2024-02-13 13:41:58 +0100

    cambios

--- Asincronia/weather/script.js
+++ Asincronia/weather/script.js
@@ -2,21 +2,38 @@
 
 let divBuscar=document.getElementById("buscador");
 let btnBuscar=document.getElementById("btnBuscar");
-
+let tiempo;
 let datos=document.getElementById("datos");
+let div_lupa=document.getElementById("div_lupa");
 
-btnBuscar.addEventListener("click",()=> mostrarDatos())
+let arrDatos=["tiempo","temp","sensacion","humedad","viento","presion"];
 
+let arrDiccionario= {
+    "Clouds":"Nublado",
+    "Clear":"Despejado",
+    "Mist":"Niebla",
+    "Drizzle":"Llovizna",
+    "Rain":"Lluvia",
+    "Snow":"Nieve",
+    "Thunderstorm":"Tormenta"
 
+};
 let divContenedor=document.getElementById("contenedor");
 
 let imagen=document.createElement("img");
 imagen.setAttribute("id","imagenValor");
 
 let imagenTiempo=   document.createElement("img");
+
+btnBuscar.addEventListener("click",()=> {mostrarDatos()
+    divContenedor.style.transition="0.5s";
+    divContenedor.style.height= "60vh" ;
+
+})
+
+
+
 function mostrarDatos(ubicacion){
-
-
 
     let APIKey='fab39eb2123e1f1999354d978289ff8c';
 
@@ -43,7 +60,10 @@ function mostrarDatos(ubicacion){
         if (response.cod === '404' || response.cod === '400' ) {
 
             imagen.setAttribute("src","img/404.png")
-
+            datos.style.height="200px";
+            imagen.style.width="250px";
+            imagen.style.height="210px";
+            imagen.style.marginTop="15%";
             datos.appendChild(imagen);
             //contenedor.removeChild(buscar);
             console.log("err")
@@ -54,22 +74,19 @@ function mostrarDatos(ubicacion){
             let divDatos2=document.createElement("div")
 
 
-            arrDatos=["tiempo","temp","humedad","viento","presion"];
+            //Array que contiene los datos que saco de la api
+            arrApi=[
+                response.weather[0].main,
+                response.main.temp+"ºC",
+                response.main.feels_like+"ºC",
+                response.main.humidity+"%",
+                response.wind.speed+" Km/h",
+                response.main.pressure+" hPa"
+            ]
 
-            arrApi=[response.weather[0].main,response.main.temp+"ºC",response.main.humidity+"%",response.wind.speed+" Km/h",response.main.pressure+" hPa"]
-
-
-            // '/' + tiempo + '/' + clima + '.png'
-
-            let tiempo;
-           tiempo=response.weather[0].icon;
-
-            if(tiempo[2]=="d"){
-                tiempo="dia";
-            }else{
-                tiempo="noche"
-            }
-
+            tiempo=response.weather[0].icon;
+                //FUNCION QUE SE ENCARGARA DE DECIDIR SI EL TIEMPO ES DE DIA O DE NOCHE
+            tiempo=comprobarTiempo(tiempo);
 
             let imagenes={
                 Clouds:"img/"+tiempo+"/cloud.png",
@@ -77,19 +94,18 @@ function mostrarDatos(ubicacion){
                 Mist:"img/"+tiempo+"/mist.png",
                 Drizzle:"img/"+tiempo+"/drizzle.png",
                 Rain:"img/"+tiempo+"/rain.png",
-                Snow:"img/"+tiempo+"/snow.png",
+                Snow:"img/snow.png",
                 Thunderstorm:"img/"+tiempo+"/thunderstorm.png",
             }
-
 
             imagenTiempo.setAttribute("src",imagenes[response.weather[0].main])
 
             divBuscar.appendChild(imagenTiempo);
-            let div_lupa=document.getElementById("div_lupa");
 
             if(!div_lupa){
                  div_lupa=document.createElement("div");
-                div_lupa.id="div_lupa";
+                 div_lupa.id="div_lupa";
+
                 let divBuscadorNuevo=document.createElement("button");
                 divBuscadorNuevo.id="divBuscadorNuevo";
 
@@ -114,7 +130,13 @@ function mostrarDatos(ubicacion){
             for(let i =0;i<arrDatos.length;i++){
                 let h5=document.createElement("h5")
 
-                h5.innerHTML=arrDatos[i].toUpperCase()+" : "+arrApi[i];
+                        //separo el primer hijo que le meto para que use el diccionario
+                if(i === 0){
+                    h5.innerHTML=arrDatos[i].toUpperCase()+" : "+arrDiccionario[arrApi[i]];
+                }else{
+                    h5.innerHTML=arrDatos[i].toUpperCase()+" : "+arrApi[i];
+                }
+
                 h5.id="h5-"+arrDatos[i];
                 if (i < 3){
                     divDatos1.appendChild(h5)
@@ -136,3 +158,13 @@ function mostrarDatos(ubicacion){
 
 
 
+function comprobarTiempo(tiempo){
+
+    if(tiempo[2]==="d"){
+        tiempo="dia";
+    }else{
+        tiempo="noche"
+    }
+
+    return tiempo;
+}
